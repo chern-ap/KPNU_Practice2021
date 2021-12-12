38eb5ba3663253cccda38f08e590504c4a7a4b86
chernavskyii <79132188+chernavskyii@users.noreply.github.com>
2021-12-12 21:01:43 +0200

    Delete C/8 kyu/6 Calculate BMI directory

--- C/8 kyu/6 Calculate BMI/main.c
+++
@@ -1,8 +0,0 @@
-const char *bmi (int weight, double height)
-{
-  double result = (weight/(height*height));
-  if (result <= 18.5) return "Underweight";
-  else if (result > 18.5 && result <= 25.0) return "Normal";
-  else if (result > 25.0 && result <=  30.0) return "Overweight";
-  else return "Obese";
-}

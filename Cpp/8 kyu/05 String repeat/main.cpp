0386f6e3de5d9a18bb3a604ff9c3318c6cb75231
chernavskyii <79132188+chernavskyii@users.noreply.github.com>
2021-12-16 19:23:05 +0200

    Create main.cpp

---
+++ Cpp/8 kyu/05 String repeat/main.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+std::string repeat_str(size_t repeat, const std::string& str) {
+  std::string result;
+  for (size_t i = 0; i < repeat; i++)
+    {
+        result = result.append(str);
+    }
+  return result;
+}

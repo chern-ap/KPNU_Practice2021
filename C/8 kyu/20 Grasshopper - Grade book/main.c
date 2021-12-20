c8df0a8a4204eb1bd70dad388620461fed47476a
chernavskyii <79132188+chernavskyii@users.noreply.github.com>
2021-12-20 20:27:24 +0200

    Create main.c

---
+++ C/8 kyu/20 Grasshopper - Grade book/main.c
@@ -0,0 +1,8 @@
+char get_grade(int a, int b, int c) {
+int avg = (a + b + c)/3;
+  if (avg >= 90) return 'A';
+  else if (avg >= 80 && avg < 90) return 'B';
+  else if (avg >= 70 && avg < 80) return 'C';
+  else if (avg >= 60 && avg < 70) return 'D';
+  else return 'F';
+}

3442ec4521aea798093de0583b2d490626bc75bd
chernavskyii <79132188+chernavskyii@users.noreply.github.com>
2021-12-22 02:31:51 +0200

    Create main.java

---
+++ Java/7 kyu/01 Vowel Count/main.java
@@ -0,0 +1,9 @@
+public class Vowels {
+
+  public static int getCount(String str) {
+    int count = 0;
+    for(char c : str.toCharArray())
+    count += (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ? 1 : 0;
+    return count;
+  }
+}
